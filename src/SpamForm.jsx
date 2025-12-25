db5518286015e25349219528e443f6f5d78892de
Nitish Pal <nitish.pal1506@gmail.com>
2025-12-25 19:20:05 +0530

    Update backend API URL for production

--- src/SpamForm.jsx
+++ src/SpamForm.jsx
@@ -19,7 +19,7 @@ function SpamForm() {
     setResult(null);
 
     try {
-      const response = await fetch("http://localhost:5000/predict", {
+      const response = await fetch("https://spam-email-backend.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
